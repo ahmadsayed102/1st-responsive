d1b19e6eef4bf3e5e85cb4a723b3a54d7cf06329
ahmadsayed102 <85335965+ahmadsayed102@users.noreply.github.com>
2022-04-22 17:52:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+ let x = document.querySelector('.ahmed');
+ function phones(phone){
+     x.src = phone;
+ }
+ let y = document.querySelector('.bo');
+ let z = document.querySelector('.container');
+ function colors (color){
+     y.style.background = color ;
+     z.style.background = color ;
+
+ }
